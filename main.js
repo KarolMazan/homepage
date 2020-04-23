680da75dd882f7064bb38f1612ed9f7d54f5dd8c
Karol Mazan <mazankarol@gmail.com>
2020-04-23 15:50:25 +0200

    BEM

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ const name = 'Karol'
 const surname = 'Mazan'
 
 console.log(`Nazywam się ${name} ${surname} i miło mi że tu zagladasz`)
+
+const mainHeader = document.querySelector(`.mainHeader`)
+console.log(mainHeader) 
