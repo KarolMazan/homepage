1f6c4309d458b1b62216c6ea0dffead088c24854
Karol Mazan <mazankarol@gmail.com>
2020-05-02 17:30:37 +0200

    feat: proper navigation

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 console.log('siemanko!!!');
 const name = 'Karol'
 const surname = 'Mazan'
+const age = '20'
 
 console.log(`Nazywam się ${name} ${surname} i miło mi że tu zagladasz`)
 
-const mainHeader = document.querySelector(`.mainHeader`)
-console.log(mainHeader) 
+const title = document.querySelector(`.title`)
+console.log(title) 
+
+if (age < 20)  {
+console.log('Masz mniej niz 20 lat');
+} else if ((age >= 20) && (age <=30))  {
+console.log('Masz wiecej niz 20 lat ale mniej niz 30');
+} else {
+    console.log('jestes 31+')
+}
+
+switch (age) {
+    case 20:
+        console.log('masz rowno 20 lat');
+    break;
+    case 30:
+        console.log('masz rowno 30 lat')
+    break;
+    default:
+        console.log(`masz ${age} lat`)
+    break;
+}
